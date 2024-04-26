6c0147e4278fb598ba84fada52310cb1bfab4a29
MaheshBabu123 <mahesh.mits78@gmail.com>
2024-04-26 11:42:07 +0530

    Mahesh_updated_json_files

--- src/config/config.php
+++ src/config/config.php
@@ -28,7 +28,7 @@ return [
 
     'stubs' => [
         'enabled' => false,
-        'path' => base_path('packages/MakePackager/src/Commands/stubs'),
+        'path' => base_path('vendor/ifo/laravel-packager/src/Commands/stubs'),
         'files' => [
             'routes/web' => 'src/routes/web.php',
             'routes/api' => 'src/routes/api.php',
